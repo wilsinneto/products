cc07f59b965e6056d4931ac591aecbe2744649ee
wilson Neto <wngaspar10@gmail.com>
2023-02-12 19:10:32 -0300

    feat: implemented save and remove product in service layer

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { ProductDto } from './product-dto';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async saveProduct(payload: string) {
+    const product: ProductDto = JSON.parse(payload);
+
+    await this.cacheManager.set(product.id, payload);
+  }
+
+  async removeProduct(id: string) {
+    await this.cacheManager.del(id);
   }
 }
