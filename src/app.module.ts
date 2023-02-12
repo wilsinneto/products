59d51001f7cf22bf7c011df439b7c08dc99b5a3a
wilson Neto <wngaspar10@gmail.com>
2023-02-12 19:11:29 -0300

    feat: register cache module with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
